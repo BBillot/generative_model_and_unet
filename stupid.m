79b07e38947a26b5551324fd2f325ffc00cbfebf
BBillot <benjamin.billot16@imperial.ac.uk>
2017-12-13 22:05:29 +0000

    deleted files

--- stupid.m
+++
@@ -1,9 +0,0 @@
-X=makeStructuredNoise(128,128, 5, 4, 2, 4, 0.1, 2, 5);
-figure; imagesc(X); colormap(gray);
-
-for m=4:10
-    MaskSize = ceil(radius*m/10);
-    g2 = fspecial('gaussian',MaskSize,v);
-    dist2 = conv2(dist,g2,'same');
-    figure; imagesc(dist2); colormap(gray);
-end
