c82d9a6490381a9ac5b334c1181753d59e6ac597
BBillot <benjamin.billot16@imperial.ac.uk>
2017-12-22 17:11:02 +0000

    removed tic toc

--- Generative_Model/PixDistanceToAxon2.m
+++ Generative_Model/PixDistanceToAxon2.m
@@ -9,16 +9,16 @@ mx = max(height+1,width+1);
 variations(AxonGTPoints(1,:)<1 |AxonGTPoints(2,:)<1 | AxonGTPoints(1,:)>height |AxonGTPoints(2,:)>width) = [];
 AxonGTPoints(:,any(AxonGTPoints<1)) = mx;
 AxonGTPoints(:,AxonGTPoints(1,:) > height | AxonGTPoints(2,:)> width) = [];
-tic
+
 BranchDist=sqrt(( repmat(Y(:),[1,size(AxonGTPoints,2)]) - repmat(AxonGTPoints(1,:), [size(X(:),1),1]) ).^2 + ...
     (repmat(X(:),[1,size(AxonGTPoints,2)]) - repmat(AxonGTPoints(2,:), [size(X(:),1),1]) ).^2);
-toc
+
 [BranchDist,BranchVariations] = min(BranchDist,[],2);
 BranchDist(BranchDist>thickness) = Inf;
 
 BranchDist = reshape(BranchDist,[height,width]);
 BranchVariations = reshape(BranchVariations, [height,width]);
-toc
+
 for i=1:height
     for j=1:width
         if BranchDist(i,j)<Inf
